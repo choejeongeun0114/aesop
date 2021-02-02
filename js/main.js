12009f7d37209a7366a04abab3d764d573a86348
choejeongeun0114 <77033225+choejeongeun0114@users.noreply.github.com>
2021-02-02 09:30:11 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    $('.mainCon04 ul').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
+    
+})
